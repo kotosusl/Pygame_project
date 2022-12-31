eed5e16bd59e49ce8de198615165e6c208a4a20d
kotosusl <kotosusl@gmail.com>
2022-12-31 19:43:06 +0700

    пыталась сделать управление, но что-то пошло не так...

--- Player.py
+++ Player.py
@@ -1,15 +1,29 @@
 import pygame
+import math
 from load_image import load_image
 
 
 class Player(pygame.sprite.Sprite):
+    image = load_image('player1.png', -1)
 
     def __init__(self, *group):
         super().__init__(*group)
-        self.image = load_image('player1.png', -1)
-        #self.image = pygame.transform.scale(self.image, (100, 50))
+        self.image = Player.image
         self.rect = self.image.get_rect()
         self.rect.x = 500
         self.rect.y = 400
-        #self.mask = pygame.mask.from_surface(self.image)
+        self.route = 0
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, *args) -> None:
+        for event in args:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    self.route = (self.route + 2) % 360
+                    self.image = pygame.transform.rotate(Player.image, self.route)
+                if event.key == pygame.K_d:
+                    self.route = (self.route - 2) % 360
+                    self.image = pygame.transform.rotate(Player.image, self.route)
+                if event.key == pygame.K_w:
+                    self.rect.x += math.sin(math.radians(self.route)) * 2
 
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
+
+size = 1000, 800
+screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+
 from load_image import load_image
 from Player import Player
 
 
 if __name__ == '__main__':
     pygame.init()
-    size = 1000, 800
-    screen = pygame.display.set_mode(size)
-    all_sprites = pygame.sprite.Group()
+
     fon_map = load_image('map\\fon1.jpg')
     fon_map = pygame.transform.scale(fon_map, (1000, 800))
     pygame.display.set_caption('Anticoronavirus')
@@ -17,8 +20,8 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            all_sprites.update(event)
         screen.blit(fon_map, (0, 0))
         all_sprites.draw(screen)
-        all_sprites.update()
         pygame.display.flip()
     pygame.quit()
