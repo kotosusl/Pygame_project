9d5ed1ad398866d857bd442fc7e24588f378a76c
kotosusl <kotosusl@gmail.com>
2023-02-07 17:08:29 +0700

    теперь точно кат сцена

--- Cut_scene.py
+++ Cut_scene.py
@@ -2,6 +2,28 @@ import pygame
 from load_image import load_image
 from befor_init import screen
 
+doctor_sprite = pygame.sprite.Group()
+
+
+class Doctor(pygame.sprite.Sprite):
+    images = [load_image('cut_scene\\doctor1.png', -1),
+              load_image('cut_scene\\doctor2.png', -1),
+              load_image('cut_scene\\doctor3.png', -1)]
+
+    def __init__(self, *group):
+        super(Doctor, self).__init__(*group)
+        self.image = Doctor.images[0]
+        self.rect = Doctor.images[1].get_rect()
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.route = 0
+
+    def new_image(self, indx):
+        self.image = Doctor.images[indx - 1]
+
+    def update(self, *args) -> None:
+        self.rect = self.image.get_rect(center=self.rect.center)
+        self.image = pygame.transform.rotate(Doctor.images[1], 360 - self.route)
+
 
 def play_cut_scene():
 
@@ -10,17 +32,14 @@ def play_cut_scene():
     end_cut_scene = pygame.USEREVENT + 4
     pygame.time.set_timer(doctor_timer, 2000)
     pygame.time.set_timer(new_viruses, 3000)
-    pygame.time.set_timer(end_cut_scene, 15000)
+    pygame.time.set_timer(end_cut_scene, 12000)
     talking = pygame.mixer.Sound('sounds/Talking.wav')
     fon_image = pygame.transform.scale(load_image('cut_scene\\fon.png'), (1000, 800))
     viruses = load_image('cut_scene\\viruses0.png', -1)
-    doctor = load_image('cut_scene\\doctor1.png', -1)
+    doctor = Doctor(doctor_sprite)
     fon_image.blit(viruses, (212 - viruses.get_rect().w // 2, 405 - viruses.get_rect().h // 2))
-    doctor_x = 700
-    fon_image.blit(doctor, (doctor_x, 500 - doctor.get_rect().h))
+    doctor.rect.x, doctor.rect.y = 700, 500 - doctor.rect.h
     move_doctor = False
-    doctor_route = 0
-    doctor_x = 700
     talking.play()
     count_images = 1
     doctor_state = 2
@@ -35,17 +54,16 @@ def play_cut_scene():
                 fon_image = pygame.transform.scale(load_image('cut_scene\\fon.png'), (1000, 800))
                 viruses = load_image(f'cut_scene\\viruses{count_images}.png')
                 fon_image.blit(viruses, (300 - viruses.get_rect().w // 2, 405 - viruses.get_rect().h // 2))
-                fon_image.blit(doctor, (doctor_x, 500 - doctor.get_rect().h))
                 count_images += 1
                 if count_images == 8:
                     pygame.time.set_timer(new_viruses, 0)
             if event.type == doctor_timer:
                 if doctor_state == 4:
-                    doctor = load_image('cut_scene\\doctor2.png', -1)
+                    doctor.new_image(2)
                     pygame.time.set_timer(doctor_timer, 0)
                     move_doctor = True
                 else:
-                    doctor = load_image(f'cut_scene\\doctor{doctor_state}.png', -1)
+                    doctor.new_image(count_images)
                     if doctor_state == 2:
                         pygame.time.set_timer(doctor_timer, 2500)
                     elif doctor_state == 3:
@@ -54,20 +72,21 @@ def play_cut_scene():
 
                 fon_image = pygame.transform.scale(load_image('cut_scene\\fon.png'), (1000, 800))
                 fon_image.blit(viruses, (300 - viruses.get_rect().w // 2, 405 - viruses.get_rect().h // 2))
-                fon_image.blit(doctor, (doctor_x, 500 - doctor.get_rect().h))
             if event.type == end_cut_scene:
                 return True
         if move_doctor:
-            if doctor_route != 180:
-                doctor_route = (doctor_route + 1) % 360
-                doctor = pygame.transform.rotate(load_image('cut_scene\\doctor2.png', -1), doctor_route)
+            if doctor.route != 180:
+                doctor.route = (doctor.route + 3) % 360
+                doctor.rect = doctor.image.get_rect(center=doctor.rect.center)
+                doctor.image = pygame.transform.rotate(load_image('cut_scene\\doctor2.png', -1), doctor.route)
+
             else:
-                doctor_x += 1
+                doctor.rect.x += 3
 
             fon_image = pygame.transform.scale(load_image('cut_scene\\fon.png'), (1000, 800))
             fon_image.blit(viruses, (300 - viruses.get_rect().w // 2, 405 - viruses.get_rect().h // 2))
-            fon_image.blit(doctor, (doctor_x, 500 - doctor.get_rect().h))
         screen.blit(fon_image, (0, 0))
+        doctor_sprite.draw(screen)
         pygame.display.flip()
 
 
